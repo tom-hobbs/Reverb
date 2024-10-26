c718fabc9630f84ed1ffb824bfac145b74ddde6b
thomas.hobbs <tomhobbs2@outlook.com>
2024-10-26 03:45:17 +0200

    run.py won't save very large files

--- run.py
+++ run.py
@@ -22,7 +22,11 @@ def run_action():
         float(entry7.get()),
         float(entry8.get()),
     )
-    write(output_path, wet_audio, sample_rate)
+    if wet_audio.shape[0] < 13230000
+        write(output_path, wet_audio, sample_rate)
+    else
+        print("!!!! WARNING very large output file !!!!")
+        print("file not saved")
 
 
 root = tk.Tk()
